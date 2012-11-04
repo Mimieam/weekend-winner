5454786cb93b6b0bd304f30586d06b71ef3275ff
Eric Caron <eric.caron@gmail.com>
2012-11-04 14:50:47 -0600

    Uploaded Shruti's setting pages

--- settings.php
+++ settings.php
@@ -9,6 +9,45 @@ $topics = $user->getTopics();
 $topicAction = false;
 include 'templates/loggedin-header.php';
 ?>
-<h1>HOLD ON</h1>
+	<div style="padding-top:100px">
+	    <h2>User Settings</h2>
+	    <form class="form-horizontal">
+	      <div class="control-group">      
+	         <label class="control-label" for="email">Email :</label>   
+	         <div class="controls">  
+	         	<label class="radio inline">
+	           		<input type="radio" name="emailRadio" id="emailRadio1" value="" checked>On
+	           	</label>
+	           	<label class="radio inline">
+		           <input type="radio" name="emailRadio" id="emailRadio2" value="">Off
+		          </label>
+	         </div>
+	      </div>
+	       
+	      <div class="control-group">        
+	         <label class="control-label" for="reminder">Reminder :</label>
+	        <div class="controls">  
+        	<label class="radio inline">
+	          	<input type="radio" name="reminderRadio" id="reminderRadio1" value="" checked>On
+		      </label>
+		      <label class="radio inline">
+		          <input type="radio" name="reminderRadio" id="reminderRadio2" value="">Off 
+		       </label>
+	        </div>
+	      </div>
+	  
+	      <div class="control-group">     
+	          <label class="control-label" for="geoLocation">Geo Location :</label>
+	         <div class="controls">  
+				<label class="radio inline">
+	          		<input type="radio" name="geoLocationRadio" id="geoLocationRadio1" value="" checked>On
+	          </label>
+				<label class="radio inline">
+	          		<input type="radio" name="geoLocationRadio" id="geoLocationRadio2" value="">Off  
+	          </label>
+	      </div>
+	    </div> 
+    </form>
+   </div>
 <?php
 include 'templates/loggedin-footer.php';
